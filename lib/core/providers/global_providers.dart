bdffe9d0d963744bd18561746fd9226f79ec893b
Deepraj Baidya <deeprajbaidya@gmail.com>
2025-03-06 05:00:14 +0530

    ♻️ (refactor): change provider naming convention

--- lib/core/providers/global_providers.dart
+++ lib/core/providers/global_providers.dart
@@ -2,19 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+final firebaseAuthInstanceProvider = Provider<FirebaseAuth>((ref) {
   return FirebaseAuth.instance;
 });
 
-final firestoreProvider = Provider<FirebaseFirestore>((ref) {
+final firestoreInstanceProvider = Provider<FirebaseFirestore>((ref) {
   return FirebaseFirestore.instance;
 });
 
-final userSessionProvider = Provider((ref) {
+final userSessionInstanceProvider = Provider((ref) {
   return FirebaseAuth.instance.currentUser;
 });
 
 final authStateProvider = StreamProvider<User?>((ref) {
-  final authState = ref.watch(firebaseAuthProvider).authStateChanges();
+  final authState = ref.watch(firebaseAuthInstanceProvider).authStateChanges();
   return authState;
 });
--- lib/core/router/app.router.dart
+++ lib/core/router/app.router.dart
@@ -1,23 +1,25 @@
+import 'package:commitz/features/error/pages/error.page.dart';
 import 'package:commitz/features/home/pages/home.page.dart';
+import 'package:commitz/features/project/pages/project.page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../features/auth/pages/landing.page.dart';
+import '../../features/auth/pages/auth.page.dart';
 import '../providers/global_providers.dart';
 
 part 'app.router.g.dart';
 
 @riverpod
-GoRouter router(RouterRef ref) {
+GoRouter router(ref) {
   final navigatorKey = GlobalKey<NavigatorState>();
-  final authStatus = ref.watch(firebaseAuthProvider);
+  final authStatus = ref.watch(firebaseAuthInstanceProvider);
   final authState = ref.watch(authStateProvider.stream);
   return GoRouter(
     navigatorKey: navigatorKey,
     debugLogDiagnostics: true,
-    initialLocation: LandingPage.route,
+    initialLocation: AuthPage.route,
     routes: [
       ShellRoute(
         builder: (BuildContext context, GoRouterState state, Widget child) {
@@ -28,27 +30,37 @@ GoRouter router(RouterRef ref) {
         },
         routes: [
           GoRoute(
-            path: LandingPage.route,
-            builder: (context, state) => LandingPage(),
+            path: AuthPage.route,
+            builder: (context, state) => AuthPage(),
           ),
           GoRoute(
             path: HomePage.route,
             builder: (context, state) => HomePage(),
           ),
-          // GoRoute(
-          //   path: '/project/:id',
-          //   builder:
-          //       (context, state) =>
-          //           ProjectDetailPage(projectId: state.pathParameters['id']!),
-          // ),
+          GoRoute(
+            path: '/project',
+            name: '/project',
+            builder: (context, state) {
+              final isLoggedin =
+                  ref.read(firebaseAuthInstanceProvider).currentUser != null;
+              if (!isLoggedin) {
+                return AuthPage();
+              }
+              final projectid = state.uri.queryParameters['id'];
+              return ProjectPage(id: projectid ?? "");
+            },
+          ),
         ],
       ),
     ],
+    errorBuilder: (context, state) {
+      return ErrorPage();
+    },
     redirect: (context, state) {
       final isLoggedin = authStatus.currentUser != null;
-      final isLoggingIn = state.matchedLocation == LandingPage.route;
+      final isLoggingIn = state.matchedLocation == AuthPage.route;
       if (!isLoggedin && !isLoggingIn) {
-        return LandingPage.route;
+        return AuthPage.route;
       }
       if (isLoggedin && isLoggingIn) {
         return HomePage.route;
